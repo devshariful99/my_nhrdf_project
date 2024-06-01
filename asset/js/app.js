e61e3c0a4b8c41e8d742c179251aac7535ebc3c3
dev-sumon <mdsharifulislamm.info@gmail.com>
2024-06-01 15:17:47 +0600

    FAQ Page --Done

--- asset/js/app.js
+++ asset/js/app.js
@@ -5,6 +5,26 @@ function ut() {
   document.getElementById("date").innerHTML = d.toLocaleDateString();
 }
 
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('faq-title') || event.target.classList.contains('title') ) {
+  
+    document.querySelectorAll('span.angle-active').forEach(function(span) {
+      span.classList.remove('angle-active');
+    });
+
+    document.querySelectorAll('.faq-desc').forEach(function(desc) {
+      desc.classList.remove('faq-desc-active');
+    });
+
+    event.target.querySelector('span').classList.add('angle-active');
+
+    var nextDesc = event.target.nextElementSibling;
+    if (nextDesc && nextDesc.classList.contains('faq-desc')) {
+      nextDesc.classList.add('faq-desc-active');
+    }
+  }
+});
+
 
 
 
